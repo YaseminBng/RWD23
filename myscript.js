70cbf8887e6fc21e7fb61ab772f48105306042bb
YaseminBng <80950611+YaseminBng@users.noreply.github.com>
2023-06-22 22:25:18 +0200

    update

---
+++ myscript.js
@@ -0,0 +1,50 @@
+const fhero = {
+    1:'./1_HEADER_Würfel/Header_1.png',
+    2:'./1_HEADER_Würfel/Header_2.png',
+    3:'./1_HEADER_Würfel/Header_3.png',
+    4:'./1_HEADER_Würfel/Header_4.png',
+}
+
+const step = 3;
+
+function trackScrollPosition() {
+  const y = window.scrollY;
+  const label = Math.min(Math.floor(y/150) + 1, 4);
+  const imageToUse = fhero[label];
+  // Change the background image
+  $('.image-container').css('background-image', `url("${imageToUse}")`);
+}
+
+$(document).ready(()=>{
+  $(window).scroll(()=>{
+    trackScrollPosition();
+  })
+})
+
+function changeImage() {
+    if (document.getElementById("imgClickAndChange").src == "http://127.0.0.1:3000/4_INTERVIEW/layout_dice.png"){
+        document.getElementById("imgClickAndChange").src = "./4_INTERVIEW/dice.png";
+    } else {
+        document.getElementById("imgClickAndChange").src = "./4_INTERVIEW/layout_dice.png";
+    }
+}
+
+function chooseBox1() {
+    document.getElementById("box").src = "./3_CHOOSEABOX/ChooseABox_1.png";
+    
+}
+
+function chooseBox2() {
+    document.getElementById("box").src = "./3_CHOOSEABOX/ChooseABox_2.png";
+
+}
+
+function clickBox() {
+    if (document.getElementById("box").src == "http://127.0.0.1:3000/3_CHOOSEABOX/ChooseABox_1.png"){
+        document.getElementById("box").src = "./3_CHOOSEABOX/ChooseABox_1+.gif";
+    }else if (document.getElementById("box").src == "http://127.0.0.1:3000/3_CHOOSEABOX/ChooseABox_2.png"){
+        document.getElementById("box").src = "./3_CHOOSEABOX/ChooseABox_2+.gif";
+    } else{
+        document.getElementById("box").src = "./3_CHOOSEABOX/ChooseABox_0.png";
+    }
+}
